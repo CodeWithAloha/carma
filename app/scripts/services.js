0929b002a5280b4a6bc605c1f92d5a6e0e3f7151
George Lee <keoki.lee@gmail.com>
2015-05-23 11:26:47 -1000

    Remove unused service that is unused.

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -13,17 +13,3 @@ angular.module('carma.services', [])
     }
 
   });
-
-(function() {
-angular.module('carma.checkins', ['firebase'])
-  .factory('CheckIn', CheckIn);
-
-CheckIn.$inject = ['$firebaseArray'];
-
-function CheckIn($firebaseArray) {
-  var ref = new Firebase('https://carma.firebaseio.com/checkins');
-
-  return $firebaseArray(ref);
-}
-
-})();
