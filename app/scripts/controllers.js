13a45a17415ed2def174edc34f568d9e562cf6e5
Russell Vea <russellbvea@gmail.com>
2015-05-23 15:18:56 -1000

    Checkin on karma button click.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,18 +2,10 @@ angular.module('carma.controllers', ['firebase'])
 .controller('DashCtrl', function($scope, Spaces, $http, $rootScope,  $firebaseArray, Restrictions) {
 
   var service = Restrictions;
-
-  var ref = new Firebase('https://carma.firebaseio.com/checkins');
-  $scope.checkins = $firebaseArray(ref);
-
+  
   var success = function(position) {
     $rootScope.center = [position.coords.latitude, position.coords.longitude];
     $scope.$emit('positionCoordinated');
-    $scope.checkins.$add({
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-      timestamp: Firebase.ServerValue.TIMESTAMP
-    });
 
     var canPark = service.canPark(position.coords.latitude, position.coords.longitude);
     canPark.then(function (result) {
@@ -29,20 +21,6 @@ angular.module('carma.controllers', ['firebase'])
     .success(function(data) {
         Spaces.set(data);
         $scope.$emit('dataLoaded');
-        $scope.features = data.features;
+        $rootScope.features = data.features;
     });
-})
-
-.controller('SpacesCtrl', function($scope, Spaces) {
-    console.log(Spaces.all());
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Spaces) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
 });
--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -1,5 +1,5 @@
-var app = angular.module('carma.directives', [])
-  app.directive('carmaMap', function($rootScope, $http) {
+var app = angular.module('carma.directives', ['firebase'])
+  app.directive('carmaMap', function($rootScope, $http, $firebaseArray) {
     return {
       scope: false,
       link: function(scope, elm, attr) {
@@ -8,13 +8,30 @@ var app = angular.module('carma.directives', [])
             .success(function(config) {
               L.mapbox.accessToken = config.access_token;
               scope.map = L.mapbox.map(elm[0], 'russellbvea.m8ni14il', {
-                  zoomControl: false,
+                  // zoomControl: false,
               });
-              scope.map.setView($rootScope.center, 18);
+              window.map = scope.map;
+              var center = [21.31296713802774, -157.86324620246884];
+              scope.map.setView(center, 19);
+
+              L.marker(center , {
+                  icon: L.mapbox.marker.icon( {
+                    'marker-color': '7D8C93'
+                  })
+              }).addTo(scope.map);
             });
         });
+
+        var ref = new Firebase('https://carma.firebaseio.com/checkins');
+        scope.checkins = $firebaseArray(ref);
+
         document.querySelector('.carma-timer-button').addEventListener('click', function() {
             document.querySelector('.carma').classList.add('drag');
+            scope.checkins.$add({
+              latitude: $rootScope.center[0],
+              longitude: $rootScope.center[1],
+              timestamp: Firebase.ServerValue.TIMESTAMP
+            });
         });
         document.querySelector('.carma-map-close').addEventListener('click', function() {
             document.querySelector('.carma').classList.remove('drag');
